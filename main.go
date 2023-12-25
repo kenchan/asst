ce41da88ea455b53cae7f2a4d102a8408282ac5e
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-12-25 12:02:53 +0900

    Use Assistant API instead of Completion API

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/slack-go/slack"
@@ -11,24 +12,88 @@ import (
 	"github.com/slack-go/slack/socketmode"
 )
 
-func completion(client *openai.Client, prompt string) (string, error) {
-	resp, err := client.CreateChatCompletion(
+func answer(client *openai.Client, prompt string, openaiAssistantID string) (string, error) {
+	threadID, err := createThread(client, prompt)
+	if err != nil {
+		return "OpenAI API Error!!", err
+	}
+
+	runID, err := createAssistantRun(client, threadID, openaiAssistantID)
+	if err != nil {
+		return "OpenAI API Error!!", err
+	}
+
+	message, err := getMessage(client, threadID, runID)
+	if err != nil {
+		return "OpenAI API Error!!", err
+	}
+
+	return message, nil
+}
+
+
+func createThread(client *openai.Client, prompt string) (string, error) {
+	resp, err := client.CreateThread(
 		context.TODO(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo1106,
-			Messages: []openai.ChatCompletionMessage{
+		openai.ThreadRequest{
+			Messages: []openai.ThreadMessage{
 				{
-					Role: openai.ChatMessageRoleUser,
+					Role: openai.ThreadMessageRoleUser,
 					Content: prompt,
 				},
 			},
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
+	}
+	return resp.ID, nil
+}
+
+func createAssistantRun(client *openai.Client, threadID string, assistantID string) (string, error) {
+	resp, err := client.CreateRun(
+		context.TODO(),
+		threadID,
+		openai.RunRequest{
+			AssistantID: assistantID,
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+	return resp.ID, nil
+}
+
+func getMessage(client *openai.Client, threadID string, runID string) (string, error) {
+	for {
+		resp, err := client.RetrieveRun(
+			context.TODO(),
+			threadID,
+			runID,
+		)
+		if err != nil {
+			return "OpenAI API Error!!", err
+		}
+		if resp.Status == "completed" {
+			break
+		}
+		time.Sleep(5 * time.Second)
+	}
+
+	resp, err := client.ListMessage(
+		context.TODO(),
+		threadID,
+		nil,
+		nil,
+		nil,
+		nil,
+	)
+
+	if err != nil {
 		return "OpenAI API Error!!", err
 	}
-	return resp.Choices[0].Message.Content, nil
+
+	return resp.Messages[0].Content[0].Text.Value, nil
 }
 
 func main() {
@@ -36,6 +101,7 @@ func main() {
 	botToken := os.Getenv("SLACK_BOT_TOKEN")
 	debugEnv := os.Getenv("DEBUG")
 	openaiApiKey := os.Getenv("OPENAI_API_KEY")
+	openaiAssistantID := os.Getenv("OPENAI_ASSISTANT_ID")
 
 	isDebug := false
 	if debugEnv != "" {
@@ -66,13 +132,14 @@ func main() {
 					log.Printf("Ignored %+v\n", evt)
 					continue
 				}
+				socketModeClient.Ack(*evt.Request)
 
 				switch eventsAPIEvent.Type {
 				case slackevents.CallbackEvent:
 					innerEvent := eventsAPIEvent.InnerEvent
 					switch ev := innerEvent.Data.(type) {
 					case *slackevents.AppMentionEvent:
-						msg, _ := completion(openaiClient, ev.Text)
+						msg, _ := answer(openaiClient, ev.Text, openaiAssistantID)
 						_, _, err := client.PostMessage(
 							ev.Channel,
 							slack.MsgOptionText(msg, false),
@@ -84,7 +151,6 @@ func main() {
 						}
 					}
 				}
-				socketModeClient.Ack(*evt.Request)
 			}
 		}
 	}()
